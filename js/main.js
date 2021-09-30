1be74ca2a109f3d718ba8794e9723b0ecb870b66
cherylleigh <90414775+cherylleigh@users.noreply.github.com>
2021-09-29 23:17:41 -0400

    Update main.js
    sources: https://observablehq.com/@molliemarie/your-first-d3-js-scatterplot
    https://www.tutorialsteacher.com/d3js/create-bar-chart-using-d3js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,14 @@ let margin = {
     right: 30,
     bottom: 35
   },
-  width = 2100 - margin.left - margin.right,
-  height = 800 - margin.top - margin.bottom;
+  width = 2000 - margin.left - margin.right,
+  height = 20000 - margin.top - margin.bottom;
+
+let svg = d3.select("body").append("svg")
+    .attr("width", 20000)
+    .attr("height", 20000)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // first visualization
 let svg1 = d3.select('#vis1')
@@ -18,7 +24,72 @@ let svg1 = d3.select('#vis1')
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
+// visual1 code
+let parseDays = d3.time.format("%D").parse;
+d3.csv("crash_catalonia.csv", function(error, data) {
+  data.forEach(function(d) {
+    d["Day of Week"] = parseDays(d["Day of Week"]);
+    d["Number of Crashes"] = + d["Number of Crashes"];
+    }
+});
+
+let xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%D"));
+
+let yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(1000);
+let yScale = d3.scale.linear().range([20000, 0])
+let xScale = d3.scale.ordinal().rangeRoundBands([0, 20000], .1)
+  
+    
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Number of Crashes");
+
+        g.selectAll(".bar")
+         .data(data)
+         .enter().append("rect")
+         .attr("class", "bar")
+         .attr("x", function(d) { return xScale(d["Day of Week"]); })
+         .attr("y", function(d) { return yScale(d["Number of Crashes"]); })
+         .attr("width", xScale.rangeBand())
+         .attr("height", function(d) { return height - yScale(d["Number of Crashes"]); });
+    });
+// code for scatterplot
+let dot = stroke: #000;
+
+
 // second visualization
+
+let margin = {
+    top: 60,
+    left: 50,
+    right: 30,
+    bottom: 35
+  },
+  width = 500 - margin.left - margin.right,
+  height = 700 - margin.top - margin.bottom;
+
+let svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 let svg2 = d3.select('#vis2')
   .append('svg')
   .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
@@ -26,10 +97,64 @@ let svg2 = d3.select('#vis2')
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
 
-CSVdata = []
-d3.csv("airtravel.csv", function(data) {
-      for (int i = 0; i < 800; i++) {
-        CSVdata.push(data[i]);
+let parseMonths = d3.time.format("%M").parse;
+d3.csv("crash_catalonia.csv", function(error, data) {
+  data.forEach(function(d) {
+    d.Month = parseMonth(d.Month)
+    d["1958"] = + d["1958"];
     }
-    drawScatterPlotChart(CSVdata);
 });
+
+  let svg = d3.select("body").append("svg") 
+      .attr("width", width)
+      .attr("height", height);
+  
+  let xScale = d3.scaleLinear()
+  .domain([1,12])
+  .range([0 , width])
+
+  let yScale = d3.scaleLinear()
+  .domain([0, 500])
+  .range([200, 700])
+  
+  let circle = svg.selectAll(dot)
+  .data(data)
+  .enter().append('circle') 
+  .attr('class', 'dot')
+  .attr('r', 10) 
+  .attr("cx", function(d) { return xScale(d.Month); })
+  .attr("cy", function(d) { return yScale(d["1958"]); });
+    
+  let text = svg.selectAll("text")
+  .data(data)
+  .enter().append("text")
+  .attr("x", function(d) { return xScale(d.Month); })
+  .attr("y", function(d) { return yScale(d["1958"]); })
+  .attr('dy', -10);
+    
+let xAxis = d3.axisBottom(xScale);
+
+let yAxis = d3.axisLeft(yScale);
+
+let xAxisGroup = svg.append("g")
+    .attr("class", "x axis")
+    .call(xAxis);
+let yAxisGroup = svg.append("g")
+    .attr("class", "y axis") 
+    .call(yAxis);
+    
+let dotGroup = svg.selectAll('dot')
+  .data(data).enter().append('g') 
+  .attr('class', 'dot')
+  .attr('transform', function(d) { return 'translate(' + xScale(d.Month) + ',' + yScale(d["1958"]) + ')'})
+
+dotGroup.append('circle')
+  .attr('class', 'dot')
+  .attr('r', 10)
+
+dotGroup.append('text')
+  .attr('class', 'dot')
+  .attr('dy', -10)
+  .text(function(d) { return d["1958"]})
+}
+      
