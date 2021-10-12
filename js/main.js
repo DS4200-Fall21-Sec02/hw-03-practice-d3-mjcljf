ab44bdf8a115112a2d2dc4a52412226d0adfe7a1
cherylleigh <90414775+cherylleigh@users.noreply.github.com>
2021-10-12 12:47:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -84,12 +84,6 @@ let margin = {
   width = 500 - margin.left - margin.right,
   height = 700 - margin.top - margin.bottom;
 
-let svg1 = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 let svg2 = d3.select('#vis2')
   .append('svg')
   .attr('preserveAspectRatio', 'xMidYMid meet') // this will scale your visualization according to the size of its parent element and the page.
